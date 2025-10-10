2b1766396de6c94d4f1c9c2b71c3bc9f6c121193
philipAthanasopoulos <filipposfivos@gmail.com>
2025-10-10 13:34:31 +0300

    added jarfile
    Signed-off-by: philipAthanasopoulos <filipposfivos@gmail.com>

--- initializr-service-sample/src/main/java/sample/service/espresso/EspressoServer.java
+++ initializr-service-sample/src/main/java/sample/service/espresso/EspressoServer.java
@@ -10,11 +10,9 @@ class EspressoServer {
     @Scheduled(fixedDelay = 2 * 60000)
     public void drinkEspresso() {
         RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = "https://initializr-nea0.onrender.com";
-        String websiteUrl = "https://start-spring-plus.onrender.com";
+        String apiUrl = "https://initializr-axm7.onrender.com";
         try {
             restTemplate.getForEntity(apiUrl, String.class);
-            restTemplate.getForEntity(websiteUrl, String.class);
             System.out.println("Poked Initializr");
         } catch (RestClientException e) {
             System.out.println("Something went wrong while drinking espresso");
